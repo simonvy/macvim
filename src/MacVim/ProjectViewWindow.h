7e0051ae8633f0a20e0ca3915b76853270a5a843
taoqian <simonvy@gmail.com>
2012-05-19 00:16:29 +0800

    optimize the projectview panel.

--- src/MacVim/ProjectViewWindow.h
+++ src/MacVim/ProjectViewWindow.h
@@ -9,18 +9,10 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ProjectViewWindow : NSWindowController <NSTableViewDataSource, NSTextDelegate> {
-    NSArray *files;
-    NSArray *filteredFiles;
 }
-
-
-@property (assign) IBOutlet NSTableView *table;
-@property (assign) IBOutlet NSSearchField *filterTextField;
 
 + (id) sharedInstance;
 - (void) loadFilesInDirectory: (NSURL *)directory;
-- (IBAction)filterChanged: (id)sender;
-- (IBAction)fileSelected:(id)sender;
 - (void) show;
 
 @end
